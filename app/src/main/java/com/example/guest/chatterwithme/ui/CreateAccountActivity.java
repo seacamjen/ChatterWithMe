19c3a5bfaf0111333486284eeaae78ddc0afb9ff
Cameron Jensen <jensenseanc@gmail.com>
2017-06-07 10:24:24 -0700

    add login to firebase in create account activity

--- app/src/main/java/com/example/guest/chatterwithme/ui/CreateAccountActivity.java
+++ app/src/main/java/com/example/guest/chatterwithme/ui/CreateAccountActivity.java
@@ -1,15 +1,112 @@
 package com.example.guest.chatterwithme.ui;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.guest.chatterwithme.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
-    @Override
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener{
+    public static final String TAG = CreateAccountActivity.class.getSimpleName();
+
+    @Bind(R.id.registerName) EditText mRegisterName;
+    @Bind(R.id.registerEmail) EditText mRegisterEmail;
+    @Bind(R.id.registerPassword) EditText mRegisterPassword;
+    @Bind(R.id.registerRePassword) EditText mRegisterRePassword;
+    @Bind(R.id.registerButton) ImageButton mRegisterButton;
+    @Bind(R.id.registerToLogin) ImageButton mRegisterToLogin;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        createAuthStateListener();
+
+        mRegisterButton.setOnClickListener(this);
+        mRegisterToLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view == mRegisterToLogin) {
+            Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+
+        if (view == mRegisterButton) {
+            createNewUser();
+        }
+    }
+
+    private void createNewUser() {
+        final String name = mRegisterName.getText().toString().trim();
+        final String email = mRegisterEmail.getText().toString().trim();
+        String password = mRegisterPassword.getText().toString().trim();
+        String confirmPassword = mRegisterRePassword.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Authentication successful");
+                        } else {
+                            Toast.makeText(CreateAccountActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
